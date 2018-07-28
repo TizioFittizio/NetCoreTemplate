443be131b6887ddf0386773ba8384b727b9490fd
C_Minor <luca.campa.94@gmail.com>
2018-07-28 23:00:18 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();               // Build an instance of asp.net core host
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()                                       // Default asp.net embedded web server
+                .UseContentRoot(Directory.GetCurrentDirectory())    // Location of the content root folder
+                .UseIISIntegration()                                // Use IIS for reverse proxy
+                .UseStartup<Startup>();                             // Include the type that contains informations for initialization
+    }
+}
